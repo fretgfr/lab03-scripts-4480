46f810fce5f602d500efa5b97594a44aebf0eec1
Robert Drescher <robert@macombhometeam.com>
2022-02-15 17:10:54 -0500

    Update C# Version

--- create_ips.cs
+++ create_ips.cs
@@ -5,19 +5,15 @@
 
 using System;
 using System.IO;
-class Create_Ips {
 
-    string BASE = "192.168.2.";
-    string OUTPUT_FILE_NAME = "Ec554_IPs.txt";
+string BASE = "192.168.2.";
+string OUTPUT_FILE_NAME = "Ec554_IPs.txt";
 
-    static void Main(string[] args) {
-        StreamWriter output_file = new StreamWriter(OUTPUT_FILE_NAME);
-        
-        for (int i = 1; i <= 254; i++) { // 0-254 inclusive
-            output_file.WriteLine(BASE + i);
-        }
+StreamWriter output_file = new StreamWriter(OUTPUT_FILE_NAME);
 
-        output_file.Close();
-        Console.WriteLine($"Output file created: {OUTPUT_FILE_NAME}");
-    }
+for (int i = 0; i <= 254; i++) { // 0-254 inclusive
+    output_file.WriteLine(BASE + i);
 }
+
+output_file.Close();
+Console.WriteLine($"Output file created: {OUTPUT_FILE_NAME}");
